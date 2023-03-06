2c0b8b3bedd7e0bc896f8f074ab9d9c94d901c53
nductoan <toanbeo090400@gmail.com>
2023-03-06 16:29:30 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
