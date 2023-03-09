a7ba14b308dd015d920335336f417f83afc22fc2
nductoan <toanbeo090400@gmail.com>
2023-03-09 13:57:26 +0700

    home-feature

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,6 +5,9 @@ import category01 from "../../assets/images/category-01.png";
 import category02 from "../../assets/images/category-02.png";
 import category03 from "../../assets/images/category-03.png";
 import category04 from "../../assets/images/category-04.png";
+import service01 from "../../assets/images/service-01.png";
+import service02 from "../../assets/images/service-02.png";
+import service03 from "../../assets/images/service-03.png";
 const Home = () => {
   return (
     <div className="home">
@@ -77,6 +80,53 @@ const Home = () => {
               <img src={category04} alt="category-row-meat" loading="lazy" />
             </div>
             <h6>Row Meat</h6>
+          </div>
+        </div>
+      </div>
+      <div className="home-feature">
+        <div className="feature-chill">
+          <div className="feature-text">
+            <h5 className="feature-subtitle">What we serve</h5>
+            <div className="title-container">
+              <h2 className="feature-title">Just sit back at home</h2>
+              <h2 className="feature-title">
+                we will <span>take care</span>
+              </h2>
+            </div>
+            <p className="feature_text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              officiis?
+            </p>
+            <p className="feature_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+              eius.{" "}
+            </p>
+          </div>
+          <div className="feature-data">
+            <div className="feature-item">
+              <img src={service01} alt="feature-img" loading="lazy" />
+              <h5>Quick Delivery</h5>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus,
+                doloremque.
+              </p>
+            </div>
+            <div className="feature-item">
+              <img src={service02} alt="feature-img" loading="lazy" />
+              <h5>Super Dine In</h5>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus,
+                doloremque.
+              </p>
+            </div>
+            <div className="feature-item">
+              <img src={service03} alt="feature-img" loading="lazy" />
+              <h5>Easy Pick Up</h5>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus,
+                doloremque.
+              </p>
+            </div>
           </div>
         </div>
       </div>
